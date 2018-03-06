a15aef207edb0ca7502b4fcbfba495d61dc8b272
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-03-06 16:46:04 +0100

    Created project structure
    Added Travis too

---
+++ DAG/stub.pyx
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import cython
+import numpy as np
+
+cimport numpy as np
+
+def dummy():
+    cdef int i = 10
+    return i
---
+++ tests/test_decoder.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from math import exp, log
+
+import numpy as np
+import pytest
+
+import DAG
+
+
+def test_dummy():
+    assert(True)
