f7b2f0dfabcd70509f3164cbef662fc93058e8e2
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-03-06 18:37:05 +0100

    Added first idea of implementation

--- DAG/__init__.py
+++ DAG/__init__.py
@@ -3,5 +3,6 @@
 # automatically compile Cython files
 import numpy as np
 import pyximport
-
 pyximport.install(setup_args={"include_dirs" : np.get_include()})
+
+import dag
--- DAG/stub.pyx
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import cython
-import numpy as np
-
-cimport numpy as np
-
-def dummy():
-    cdef int i = 10
-    return i
