c8af3c13bcb1dcd7df91d6da8d47067a2a23d3d4
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-10-22 17:51:29 +0200

    Project moved to BATMAN protocol, cleanup

--- DAG/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# automatically compile Cython files
-import numpy as np
-import pyximport
-pyximport.install(setup_args={"include_dirs" : np.get_include()})
-
-import dag
