488d2b2d0d1aa074c32b8c072801dfdc03f7c4d2
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-10-28 12:25:42 +0100

    Simple simulator, without BATMAN layer

--- dummy.py
+++ dummy.py
@@ -11,18 +11,17 @@ channel = Channel()
 
 def interarrival_gen():
     while True:
-        yield random()
+        yield random() * 10
 
 def size_gen():
     while True:
         yield randint(100, 200)
 
-
-app1 = ApplicationLayer(interarrival_gen(), size_gen(), 5, 1, 1, 2, 2)
-app2 = ApplicationLayer(interarrival_gen(), size_gen(), 5, 2, 2, 1, 1)
+app1 = ApplicationLayer(interarrival_gen(), size_gen(), 5000, 1, 1, 2, 2)
+app2 = ApplicationLayer(interarrival_gen(), size_gen(), 5000, 2, 2, 1, 1)
 
 app1.connect_lower_layer(channel)
 app2.connect_lower_layer(channel)
 
 event_queue.add(Event(action=lambda: app1.generate_pkts(), when=0))
-event_queue.add(Event(action=lambda: app2.generate_pkts(), when=0))
+# event_queue.add(Event(action=lambda: app2.generate_pkts(), when=0))
