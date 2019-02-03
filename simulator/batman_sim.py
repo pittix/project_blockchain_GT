291d2ef845385e96adeb7554f4d37f6be0fffca6
Enrico Lovisotto <enricolovisotto@gmail.com>
2019-02-03 16:34:13 +0100

    Fixed dictionary

--- simulator/batman_sim.py
+++ simulator/batman_sim.py
@@ -1,6 +1,8 @@
+import logging
+import sys
+
 from .core import *
 from .layers import *
-import logging,sys
 
 SEED=1
 square_x=10
@@ -14,8 +16,13 @@ LAMBDA_=0.1 #what's this lambda?
 def nodeCreation(node_num):
     nodes=[]
     for i in range(node_num):
-        arg=dict({'position'=[random.rand()*square_x, random.rand()*square_y],
-             'G'=GAIN,'No'=NOISE,'Pin'= P_IN,'lambda_'=LAMBDA_ })
+        arg=dict({
+            'position': [random.rand()*square_x, random.rand()*square_y],
+            'G': GAIN,
+            'No': NOISE,
+            'Pin': P_IN,
+            'lambda_': LAMBDA_
+        })
         nodes.append(Channel(seed=SEED,args))
 
     return nodes
