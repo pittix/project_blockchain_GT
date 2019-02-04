fd5b498b186fc219d1417d23ae10735a310cc1fd
Andrea Pittaro <pittaroa@dei.unipd.it>
2019-02-04 16:03:50 +0100

    Fixed indentation

--- simulator/batman_sim.py
+++ simulator/batman_sim.py
@@ -4,29 +4,28 @@ import sys
 from .core import *
 from .layers import *
 
-SEED=1
-square_x=10
-square_y=20
-GAIN=5
-NOISE=10^(-4)
-P_IN=0 #what's this?
-T_S=10^(-5)
-LAMBDA_=0.1 #what's this lambda?
+SEED = 1
+square_x = 10
+square_y = 20
+GAIN = 5
+NOISE = 10**(-4)
+P_IN = 0  # what's this?
+T_S = 10**(-5)
+LAMBDA_= 0.1  #what's this lambda?
 
 def nodeCreation(node_num):
-    nodes=[]
+    nodes = []
     for i in range(node_num):
-        arg=dict({
+        arg = dict({
             'position': [random.rand()*square_x, random.rand()*square_y],
             'G': GAIN,
             'No': NOISE,
             'Pin': P_IN,
             'lambda_': LAMBDA_
         })
-        nodes.append(Channel(seed=SEED,args))
-
+        nodes.append(Channel(seed=SEED, **arg))
     return nodes
 
-if __name__=='main':
+if __name__== 'main' :
     queue = EventQueue()
     nodes = nodeCreation(sys.argv[1])
