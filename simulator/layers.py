39be3e7a8cff7ba89d97c7db47e0e6ee330cf032
Enrico Lovisotto <enrico.lovisotto@tutanota.com>
2019-02-06 17:16:32 +0100

    Fixed signature for shortest path

--- simulator/layers.py
+++ simulator/layers.py
@@ -2,6 +2,7 @@ import logging
 from math import sqrt
 from random import random
 
+import networkx as nx
 from numpy.random import geometric
 from scipy.constants import Boltzmann, pi
 from scipy.special import erfc
@@ -154,7 +155,7 @@ class BatmanLayer(Layer):
         app_layer.lower_layer_id = self.id_
 
     def recv_from_up(self, packet, upper_layer_id):
-        path = G.shortest_path(self.local_ip, packet['dst_ip'])
+        path = nx.shortest_path(G, self.local_ip, packet['dst_ip'])
         packet['path'] = path
         self.send_down(packet,
                        self.neighbour_table[packet['path'][0]])
