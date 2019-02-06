1456d9d8d851e90b0c96ebb64abf6b59af1b9f07
Andrea Pittaro <pittaroa@dei.unipd.it>
2019-02-06 21:50:06 +0100

    written simulator runner

---
+++ simulator_starter.py
@@ -0,0 +1,32 @@
+from simulator import *
+from test_batman import *
+import argparse
+import sys
+parser = argparse.ArgumentParser("Simulate batman")
+parser.add_argument('-n', help='number of nodes', action='append',
+                dest='node_num', type=int)
+parser.add_argument('-s', help='seed', action='append', dest='s', type=int)
+parser.add_argument('-d-lim', help='distance limit', action='append',
+                dest='dist_lim', type=int)
+parser.add_argument('-d', help='length of the square', action='append',
+                dest='dim', type=int)
+parser.add_argument('-a', help='rate of connection between apps', action='append',
+                dest='app_rate', type=float)
+
+
+var = vars(parser.parse_args(sys.argv[1:]))
+# check that I have the same number of simulations
+if (len(var["node_num"]) == len(var["s"])
+        and len(var["s"]) == len(var["dim"])
+        and len(var["s"]) == len(var["dist_lim"])
+        and len(var["s"]) == len(var["app_rate"])):
+    # run a simulation for each parameter
+    for i in range(len(var["node_num"])):
+        tmp_var = {
+            "s": var["s"][i],
+            "node_num": var["node_num"][i],
+            "dim": var["dim"][i],
+            "dist_lim": var["dist_lim"][i],
+            "app_rate": var["app_rate"][i]
+            }
+        simulator_batman(**tmp_var)
--- test_batman.py
+++ test_batman.py
@@ -1,119 +1,127 @@
 import logging
 from math import exp, sqrt
-from random import randint, random, randrange, seed
-
-import matplotlib.pyplot as plt
-from scipy.constants import Boltzmann, pi
-
+from random import randint, random, seed
 from simulator import *
 
-seed(14)
-
+# default values
 NODE_NUM = 100
 DIM = 1000
 DISTANCE_LIM = 200
 LIGHT_SPEED = 299792458
 APP_CONN_RATE = 0.25
+PROC_TIME = 0.001
 
 logging.basicConfig(level=logging.ERROR)
 
-# 1) load queue for events created in simulator module
-event_queue.clean()
 
-## create a number of batman layers, corresponding to nodes
-batmans = {
-    ip: BatmanLayer(ip)
-    for ip in range(NODE_NUM)
-}
-
-## connect each other using some channels, described using a success probability
-## and round trip time
-node_positions = {}
-channels = {}
-for ip in batmans.keys():
-    node_positions[ip] = (DIM*random(), DIM*random())
-
-for ip1 in node_positions:
-    for ip2 in node_positions:
-        if ip1 == ip2:
-            continue
-        dist = sqrt((node_positions[ip1][0] - node_positions[ip2][0])**2 +
-                    (node_positions[ip1][1] -
-                     node_positions[ip2][1])**2)
-        if dist < DISTANCE_LIM:
-            p_succ = exp(-dist/DISTANCE_LIM)
-            rtt = 0.001 + dist/LIGHT_SPEED
-            local_channels = batmans[ip1].connect_to(batmans[ip2], p_succ=p_succ, rtt=rtt)
-            channels = {**channels, **local_channels}
-
-## create a couple of application for each end-to-end stream: for the simulation
-## to be reasonable, each node has to have at least one application
-
-apps = []
-port_no = 1000
 def interarrival_gen():
     while True:
         yield random() * 10
+
 
 def size_gen():
     while True:
         yield randint(100, 200)
 
-# ensure unique port for each app (just to play safe)
 
-for ip1 in node_positions:
-    for ip2 in node_positions:
-        if ip1 == ip2:
-            continue
-        elif random() < APP_CONN_RATE:
-            port1 = port_no
-            port2 = port_no + 1
-            port_no += 10
+def simulator_batman(s=1, node_num=NODE_NUM, dim=DIM, dist_lim=DISTANCE_LIM,
+                app_rate=APP_CONN_RATE):
+    # 1) load queue for events created in simulator module
+    seed(s)
 
-            app1 = ApplicationLayer(interarrival_gen(),
-                                    size_gen(),
-                                    start_time=0,
-                                    stop_time=100,
-                                    local_port=port1,
-                                    local_ip=ip1,
-                                    dst_port=port2,
-                                    dst_ip=ip2)
-            batmans[ip1].connect_app(app1)
+    event_queue.clean()
 
-            app2 = ApplicationLayer(interarrival_gen(),
-                                    size_gen(),
-                                    start_time=0,
-                                    stop_time=100,
-                                    local_port=port2,
-                                    local_ip=ip2,
-                                    dst_port=port1,
-                                    dst_ip=ip1)
-            batmans[ip2].connect_app(app2)
+    # create a number of batman layers, corresponding to nodes
+    batmans = {
+        ip: BatmanLayer(ip)
+        for ip in range(NODE_NUM)
+    }
 
-            apps.append( (app1, app2) )
+    # connect each other using some channels, described using a success probability
+    # and round trip time
+    node_positions = {}
+    channels = {}
+    for ip in batmans.keys():
+        node_positions[ip] = (dim*random(), dim*random())
 
-nx.draw(G, with_labels=True, pos=node_positions)
-plt.show()
+    for ip1 in node_positions:
+        for ip2 in node_positions:
+            if ip1 == ip2:
+                continue
+            dist = sqrt((node_positions[ip1][0] - node_positions[ip2][0])**2
+                        + (node_positions[ip1][1]
+                         - node_positions[ip2][1])**2)
+            if dist < dist_lim:
+                p_succ = exp(-dist/dist_lim)
+                rtt = PROC_TIME + dist/LIGHT_SPEED
+                local_channels = batmans[ip1].connect_to(batmans[ip2],
+                                p_succ=p_succ, rtt=rtt)
+                channels = {**channels, **local_channels}
 
-## run the simulation, until we run out of events
-counter = 0
-while True:
-    if counter % 10000 == 0:
-        print(event_queue.now)
-    counter += 1
+    # create the application for each end-to-end stream: for the simulation
+    # to be reasonable, each node has to have at least one application
 
-    # trigger events until we run out of them
-    if event_queue.next() is None:
-        break
+    apps = []
+    port_no = 1000
 
-## judge application layer rates
-performances = { ip: 0 for ip in batmans.keys() }
-cur_time = event_queue.now
-for app1, app2 in apps:
-    # node1 -> node2 communication
-    print('TX: ', app1.id_, ' -->', app2.id_, ' Rate ', app1.rx_packet_size/cur_time)
-    print('TX: ', app2.id_, ' -->', app1.id_, ' Rate ', app2.rx_packet_size/cur_time)
-    performances[app1.local_ip] += app1.rx_packet_size
-    performances[app2.local_ip] += app2.rx_packet_size
+    # ensure unique port for each app (just to play safe)
 
-print(performances)
+    for ip1 in node_positions:
+        for ip2 in node_positions:
+            if ip1 == ip2:
+                continue
+            elif random() < app_rate:
+                port1 = port_no
+                port2 = port_no + 1
+                port_no += 10
+
+                app1 = ApplicationLayer(interarrival_gen(),
+                                        size_gen(),
+                                        start_time=0,
+                                        stop_time=100,
+                                        local_port=port1,
+                                        local_ip=ip1,
+                                        dst_port=port2,
+                                        dst_ip=ip2)
+                batmans[ip1].connect_app(app1)
+
+                app2 = ApplicationLayer(interarrival_gen(),
+                                        size_gen(),
+                                        start_time=0,
+                                        stop_time=100,
+                                        local_port=port2,
+                                        local_ip=ip2,
+                                        dst_port=port1,
+                                        dst_ip=ip1)
+                batmans[ip2].connect_app(app2)
+
+                apps.append( (app1, app2) )
+
+    nx.draw(G, with_labels=True, pos=node_positions)
+    # plt.show()
+
+    # run the simulation, until we run out of events
+    counter = 0
+    while True:
+        if counter % 10000 == 0:
+            print(event_queue.now)
+        counter += 1
+
+        # trigger events until we run out of them
+        if event_queue.next() is None:
+            break
+
+    # judge application layer rates
+    performances = {ip: 0 for ip in batmans.keys()}
+    # cur_time = event_queue.now
+    for app1, app2 in apps:
+        # node1 -> node2 communication
+        # print('TX: ', app1.id_, ' -->', app2.id_, ' Rate ', app1.rx_packet_size/cur_time)
+        # print('TX: ', app2.id_, ' -->', app1.id_, ' Rate ', app2.rx_packet_size/cur_time)
+        performances[app1.local_ip] += app1.rx_packet_size
+        performances[app2.local_ip] += app2.rx_packet_size
+
+    simulation_filename = "results/{}_{}.csv".format(seed, node_num)
+    with open(simulation_filename, 'wb') as csvfile:
+        for ip, size in performances.items():
+            csvfile.write("{},{}".format(ip, size))
