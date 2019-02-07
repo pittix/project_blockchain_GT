b3c2fe23085871eb86815f6b3e25b04f7b0efbfb
Andrea Pittaro <pittaroa@dei.unipd.it>
2019-02-07 17:48:04 +0100

    added files where to put the simulation parameters and set runner

--- runner.sh
+++ runner.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-FAILED=0
-
-python -O test_batman.py -n 100 -s 17 -d-lim 500 -d 1000 -a 0.25 -st 100 -selfish 0.1 &
-python -O test_batman.py -n 100 -s 18 -d-lim 500 -d 1000 -a 0.25 -st 100 -selfish 0.1 &
-
-## wait for all the job to finish
-for job in `jobs -p`
-do
-    wait $job || let "FAILED+=1"
-done
-
-echo "$FAILED processes failed"
+python -O simulator_starter.py -n &(cat nodes.txt) -s &(cat seeds.txt) \
+      -d-lim &(cat dist-limits.txt) -d &(cat dimensions.txt) \
+      -a &(cat app_rate.txt) -st &(cat stop.txt) -selfish &(cat selfish.txt)
+# python -O test_batman.py -n 100 -s 18 -d-lim 500 -d 1000 -a 0.25 -st 100 -selfish 0.1 &
