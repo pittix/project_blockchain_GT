1e812e637263b5ef7dceb3e8399feeea723966d8
Andrea <pittix@live.it>
2019-02-08 09:53:05 +0100

    deleted space in runner. added results

--- runner.sh
+++ runner.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 python -O simulator_starter.py \
-       -n $(cat simulator_params/ nodes.txt) \
-       -s $(cat simulator_params/ seeds.txt) \
-       -d-lim $(cat simulator_params/ dist-limits.txt) \
-       -d $(cat simulator_params/ dimensions.txt) \
-       -a $(cat simulator_params/ app_rate.txt) \
-       -st $(cat simulator_params/ stop.txt) \
-       -selfish $(cat simulator_params/ selfish.txt)
+       -n $(cat simulator_params/nodes.txt) \
+       -s $(cat simulator_params/seeds.txt) \
+       -d-lim $(cat simulator_params/dist-limits.txt) \
+       -d $(cat simulator_params/dimensions.txt) \
+       -a $(cat simulator_params/app_rate.txt) \
+       -st $(cat simulator_params/stop.txt) \
+       -selfish $(cat simulator_params/selfish.txt)
