019fbd507733a5120900e12a9b69a7c681a64b2d
Enrico Lovisotto <enrico.lovisotto@tutanota.com>
2019-02-10 16:56:58 +0100

    Fixed nasty name bug in selfish_compare, moved to proper place

--- selfish_compare.py
+++ selfish_compare.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
-results = pd.read_hdf("../results/simulation_results.hdf5")
+results = pd.read_hdf("results/simulation_results.hdf5")
 
 variables = list(results.columns)
 for non_param in ('selfish_num', 'altruistic_num', 'selfish', 'altruistic'):
@@ -14,9 +14,13 @@ summary = results.groupby(variables).sum()
 
 # element-by-element division
 summary['selfish'] = summary['selfish'] / summary['selfish_num']
-summary['altruistic'] = summary['selfish'] / summary['altruistic_num']
+summary['altruistic'] = summary['altruistic'] / summary['altruistic_num']
+
+# NaNs are generated when no selfish node was present: set the selfish bitrate
+# to zero then
+summary.fillna(0, inplace=True)
 
 del summary['selfish_num']
 del summary['altruistic_num']
 
-summary.to_csv("averaged_simulations.csv")
+summary.to_csv("results/averaged_simulations.csv")
