a946fb65d8cc50b204ccb3cb17b95f176a387f25
Enrico Lovisotto <enrico.lovisotto@tutanota.com>
2019-02-11 22:40:07 +0100

    Enhanced plotter.py

--- plotter.py
+++ plotter.py
@@ -10,38 +10,45 @@ matplotlib.rc('font', **font)
 
 FIXED_PARAM = 'selfish_rate'
 
-data = pd.read_hdf("results/simulation_results.hdf5")
+data_original = pd.read_hdf("results/simulation_results_all_01.hdf")
 
-data = data[
-    (data['node_num'] == 100) &
-    (data['app_rate'] == data['app_rate'][1])
-].groupby([FIXED_PARAM]).sum()
+app_rates = list(set(data_original['app_rate']))
+node_nums = list(set(data_original['node_num']))
 
-data['selfish'] /= data['selfish_num']
-data['altruistic'] /= data['altruistic_num']
+print(app_rates)
+print(node_nums)
+for node_num in node_nums:
+    for app_rate in app_rates:
+        print(node_num, app_rate)
+        data = data_original[
+            (data_original['node_num'] == node_num) &
+            (data_original['app_rate'] == app_rate)
+        ].groupby([FIXED_PARAM]).sum()
 
-del data['s']
-del data['selfish_num']
-del data['altruistic_num']
+        data['selfish'] /= data['selfish_num']
+        data['altruistic'] /= data['altruistic_num']
 
-data = data.reset_index()
+        del data['s']
+        del data['selfish_num']
+        del data['altruistic_num']
 
-# plot obj_func vs one of the parameters
+        data = data.reset_index()
 
-fig = plt.figure(figsize=(3.5, 3), frameon=False)
-ax = fig.gca()
+        # plot obj_func vs one of the parameters
 
-data.plot(ax=ax,
-          use_index=True,
-          x=FIXED_PARAM,
-          y=['altruistic', 'selfish'],
-          style='o-',
-          title='Average node bitrate')
+        fig = plt.figure(figsize=(3.5, 3), frameon=False)
+        ax = fig.gca()
 
-ax.set(xlabel=FIXED_PARAM.replace('_', ' ').capitalize(),
-       ylabel="Objective function")
+        data.plot(ax=ax,
+                  use_index=True,
+                  x=FIXED_PARAM,
+                  y=['altruistic', 'selfish'],
+                  style='o-',
+                  title='N={}, app rate={:.2f}'.format(node_num, app_rate))
 
-plt.tight_layout()
-# plt.show()
-oth = input("suffix for file, no space allowed")
-plt.savefig('report/figures/PUNISHobj_func_vs_{}_oth.eps'.format(FIXED_PARAM, oth))
+        ax.set(xlabel=FIXED_PARAM.replace('_', ' '),
+               ylabel="Objective function")
+
+        plt.tight_layout()
+        # plt.show()
+        plt.savefig('report/figures/obj_funcs/obj_func_vs_{}_N_{}_app_{}.eps'.format(FIXED_PARAM, node_num, app_rate))
