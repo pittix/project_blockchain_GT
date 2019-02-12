a8bbfa230d8c0e9ac377443df3cd43ffec34fd15
Andrea <pittix@live.it>
2019-02-12 11:46:44 +0100

    Updated results, abstract and conclusions in report.tex

--- plotter.py
+++ plotter.py
@@ -10,7 +10,7 @@ matplotlib.rc('font', **font)
 
 FIXED_PARAM = 'selfish_rate'
 
-data_original = pd.read_hdf("results/simulation_results_all_01.hdf")
+data_original = pd.read_hdf("results/simulation_results_all_03.hdf5")
 
 app_rates = list(set(data_original['app_rate']))
 node_nums = list(set(data_original['node_num']))
@@ -38,7 +38,6 @@ for node_num in node_nums:
 
         fig = plt.figure(figsize=(3.5, 3), frameon=False)
         ax = fig.gca()
-
         data.plot(ax=ax,
                   use_index=True,
                   x=FIXED_PARAM,
@@ -48,7 +47,8 @@ for node_num in node_nums:
 
         ax.set(xlabel=FIXED_PARAM.replace('_', ' '),
                ylabel="Objective function")
+        ax.set_xlim([0.0, 0.1])
 
         plt.tight_layout()
         # plt.show()
-        plt.savefig('report/figures/obj_funcs/obj_func_vs_{}_N_{}_app_{}.eps'.format(FIXED_PARAM, node_num, app_rate))
+        plt.savefig('report/figures/obj_funcs/lovi-obj_func_vs_{}_N_{}_app_{}.eps'.format(FIXED_PARAM, node_num, app_rate))
