25b61bd9c983f9a2e814acc10299eb8e7ebf7cfd
Andrea Pittaro <pittaroa@dei.unipd.it>
2019-02-16 16:55:05 +0100

    modified plotter for new sims. needs edit
     * time intervals needs to be accumulated to avoid bad plots

--- plotter.py
+++ plotter.py
@@ -8,9 +8,9 @@ font = {'family': 'normal',
 
 matplotlib.rc('font', **font)
 
-FIXED_PARAM = 'selfish_rate'
+FIXED_PARAM = 'time'
 
-data_original = pd.read_hdf("results/simulation_results_all_03.hdf5")
+data_original = pd.read_hdf("results/simulation_results_0_2019-02-16-14-26-33.hdf5")
 
 app_rates = list(set(data_original['app_rate']))
 node_nums = list(set(data_original['node_num']))
@@ -25,12 +25,11 @@ for node_num in node_nums:
             & (data_original['app_rate'] == app_rate)
         ].groupby([FIXED_PARAM]).sum()
 
-        data['selfish'] /= data['selfish_num']
-        data['altruistic'] /= data['altruistic_num']
-
+        data['selfish_num'] /= data['node_num']
+        data['altruistic_num'] /= data['node_num']
         del data['s']
-        del data['selfish_num']
-        del data['altruistic_num']
+        # del data['selfish_num']
+        # del data['altruistic_num']
 
         data = data.reset_index()
 
@@ -41,16 +40,15 @@ for node_num in node_nums:
         data.plot(ax=ax,
                   use_index=True,
                   x=FIXED_PARAM,
-                  y=['altruistic', 'selfish'],
-                  style='o-',
+                  y=['altruistic_num', 'selfish_num'],
+                  style='.',
                   title='N={}, app rate={:.2f}'.format(node_num, app_rate))
 
         ax.set(xlabel=FIXED_PARAM.replace('_', ' '),
                ylabel="Objective function")
-        ax.set_xlim([0.0, 0.1])
+        ax.set_xlim([60.0, 62.0])
 
         plt.tight_layout()
         # plt.show()
-        plt.savefig("""report/figures/
-                    obj_funcs/lovi-obj_func_vs_{}_N_{}_app_{}
-                    .eps""".format(FIXED_PARAM, node_num, app_rate))
+        plt.savefig("report/figures/obj_funcs/lovi-obj_func_vs_{}_N_{}_app_{}\
+        .eps".format(FIXED_PARAM, node_num, app_rate))
